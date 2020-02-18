fafa27a1ebe55d1ca901dc4500ebc4b22e18bd04
clevy2013 <58232811+clevy2013@users.noreply.github.com>
2020-02-18 14:30:24 -0500

    IDL Location
    Retweaked, typo

--- modis-utils/extract_modis_hdf4.sh
+++ modis-utils/extract_modis_hdf4.sh
@@ -23,7 +23,7 @@ QA for shortwave albedo: BRDF_Albedo_Band_Mandatory_Quality_shortwave
 EOF
 
 IDL="/home/apps/harris-5.5.1/envi55/idl87/lib -quiet -e"
-###IDL="/usr/local/exelis/idl85/bin/idl -quiet -e"
+#IDL="/usr/local/exelis/idl85/bin/idl -quiet -e"
 
 OPTS=`getopt -o D:I:P: --long product_dir:,product_id:,parameter:,loc:,lat:,lon:,year:,bdoy:,edoy:,output: -n '$(basename ${0})' -- "$@"`
 if [[ $? != 0 ]]; then echo "Failed parsing options" >&2; echo "${USAGE}"; exit 1; fi
@@ -39,9 +39,9 @@ do
         -I | --product_id )
             case "${2}" in
                 "" ) shift 2 ;;
-                *) PRODUCTID=${2} ; shift 2 ;;
+                *) PRODUCTID=${2} ; shift 2 ;; 
             esac ;;
-        -P | --para meter )
+        -P | --parameter )
             case "${2}" in
                 "" ) shift 2 ;;
                 *) PARAMETER_NAMES=${2} ; shift 2 ;;
